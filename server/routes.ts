ef9fc362f8a54bdcef056a9f60e48eee09dd7c97
Dwaapar <santrazor795@gmail.com>
2025-09-14 22:24:12 +0530

    chore: inline express-session typings

--- server/routes.ts
+++ server/routes.ts
@@ -1,5 +1,13 @@
 import type { Express } from "express";
 import { createServer, type Server } from "http";
+import "express-session";
+
+declare module "express-session" {
+  interface SessionData {
+    id?: string;
+    userId?: string;
+  }
+}
 
 import { storage } from "./storage";
 import { 
